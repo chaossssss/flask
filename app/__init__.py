1e9412298d55f8f99294abe010fad54c0a02728d
chaossssss <shensichao@haishukeji.com>
2025-01-22 09:29:33 +0800

    feat:初始化

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.config.from_object("config.Config")
+
+    from . import routes
+
+    app.register_blueprint(routes.bp)
+
+    return app
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    DEBUG = True
+    SECRET_KEY = "mysecretkey"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///mydatabase.db"
